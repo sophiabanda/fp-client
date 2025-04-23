2ee6afeb2971281876189e51b2b57106a0ecc165
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2025-04-23 19:34:58 -0400

    updated endpoints for fp

--- src/main.jsx
+++ src/main.jsx
@@ -12,11 +12,11 @@ createRoot(document.getElementById('root')).render(
       loadOptions={{
         apiKey: '6mtUaNcPcmQORr5lg6wm',
         endpoint: [
-          'https://sophiabanda.work/9m9M8557TfmVm4uZ/sN476M9TIiuVVhN3',
+          'https://sophiabanda.work/kl74xUQUAFJJ4Sxw/30Bhs4rb8HIAVhJp',
           FingerprintJSPro.defaultEndpoint,
         ],
         scriptUrlPattern: [
-          'https://sophiabanda.work/9m9M8557TfmVm4uZ/hkXCizJfrDYfctbV?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>',
+          'https://sophiabanda.work/kl74xUQUAFJJ4Sxw/RR840wdo8s0n3rEg?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>',
           FingerprintJSPro.defaultScriptUrlPattern,
         ],
       }}
