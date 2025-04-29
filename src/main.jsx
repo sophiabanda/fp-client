5b17ddae3e1a64e6d69734e6ee96871f30c65d5c
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2025-04-29 14:31:43 -0400

    undo last experiment

--- src/main.jsx
+++ src/main.jsx
@@ -11,10 +11,14 @@ createRoot(document.getElementById('root')).render(
     <FpjsProvider
       loadOptions={{
         apiKey: '6mtUaNcPcmQORr5lg6wm',
-        endpoint: 'https://sophiabanda.work/kl74xUQUAFJJ4Sxw/30Bhs4rb8HIAVhJp',
-
-        scriptUrlPattern:
+        endpoint: [
+          'https://sophiabanda.work/kl74xUQUAFJJ4Sxw/30Bhs4rb8HIAVhJp',
+          FingerprintJSPro.defaultEndpoint,
+        ],
+        scriptUrlPattern: [
           'https://sophiabanda.work/kl74xUQUAFJJ4Sxw/RR840wdo8s0n3rEg?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>',
+          FingerprintJSPro.defaultScriptUrlPattern,
+        ],
       }}
     >
       <App />
